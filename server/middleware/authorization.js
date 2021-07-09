d8b2dabfa5f9259b45b940b4a64c281102002adb
ezeilo-su <ezeilo.su@gmail.com>
2021-07-09 13:52:53 +0100

    Add message sender authorization middleware in the message controller

---
+++ server/middleware/authorization.js
@@ -0,0 +1,26 @@
+const Conversation = require('../db/models/conversation');
+
+const isAuthorizedToSendToConv = async (req, res, next) => {
+  const { recipientId, text, conversationId, sender } = req.body;
+
+  const currentUser = req.user;
+
+  if (!currentUser) return res.sendStatus(401);
+
+  if (conversationId) {
+    const conv = await Conversation.findOne({
+      where: { id: conversationId },
+    });
+
+    // no conversation, return 404
+    if (!conv) return res.sendStatus(404);
+
+    // Ensure that users can send messages to only their own conversation(s)
+    if (![conv.user1Id, conv.user2Id].includes(currentUser.id))
+      // not authorized, return 403
+      return res.sendStatus(403);
+  }
+  return next();
+};
+
+module.exports = { isAuthorizedToSendToConv };
--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -1,55 +1,34 @@
 const router = require('express').Router();
 const onlineUsers = require('../../onlineUsers');
 const { Conversation, Message } = require('../../db/models');
+const { isAuthorizedToSendToConv } = require('../../middleware/authorization');
 
-// expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
-router.post('/', async (req, res, next) => {
+router.post('/', isAuthorizedToSendToConv, async (req, res, next) => {
   try {
-    if (!req.user) {
-      return res.sendStatus(401);
-    }
     const senderId = req.user.id;
     const { recipientId, text, conversationId, sender } = req.body;
+    let conversation;
 
-    // if we already know conversation id, we can save time and just add it to message and return
+    if (!conversationId) {
+      conversation = await Conversation.findConversation(senderId, recipientId);
 
-    if (conversationId) { // Ensure that users can send messages to only their own conversation(s)
-      const conv = await Conversation.findOne({
-        where: { id: conversationId },
-      });
-
-      if (conv && [conv.user1Id, conv.user2Id].includes(senderId)) {
-        const message = await Message.create({
-          senderId,
-          text,
-          conversationId,
+      if (!conversation) {
+        conversation = await Conversation.create({
+          user1Id: senderId,
+          user2Id: recipientId,
         });
-        return res.json({ message, sender });
-      } else return res.sendStatus(401);
-    }
-
-    // if we don't have conversation id, find a conversation to make sure it doesn't already exist
-    let conversation = await Conversation.findConversation(
-      senderId,
-      recipientId
-    );
-
-    if (!conversation) {
-      // create conversation
-      conversation = await Conversation.create({
-        user1Id: senderId,
-        user2Id: recipientId,
-      });
-      if (onlineUsers.includes(sender.id)) {
-        sender.online = true;
+        if (onlineUsers.includes(sender.id)) {
+          sender.online = true;
+        }
       }
     }
 
     const message = await Message.create({
       senderId,
       text,
-      conversationId: conversation.id,
+      conversationId: conversationId || conversation.id,
     });
+
     res.json({ message, sender });
   } catch (error) {
     next(error);
