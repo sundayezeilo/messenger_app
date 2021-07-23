e45671996ceda0648b20d176585c6da7b002f7d9
ezeilo-su <ezeilo.su@gmail.com>
2021-07-23 18:40:50 +0100

    Refactor socketAuth to use async/await

--- server/middleware/socketAuth.js
+++ server/middleware/socketAuth.js
@@ -1,22 +1,26 @@
 const jwt = require('jsonwebtoken');
 const { User } = require('../db/models');
+const { promisify } = require('util');
 
-const authenticateConnection = (socket, next) => {
+const authenticateConnection = async (socket, next) => {
   const token = socket.handshake.auth.token;
 
-  if (token) {
-    jwt.verify(token, process.env.SESSION_SECRET, (err, decoded) => {
-      if (err) {
-        next(new Error('invalid token'));
-      }
-      User.findOne({
-        where: { id: decoded.id },
-      })
-        .then(() => next())
-        .catch(() => next(new Error('authentication failed!')));
+  if (!token) next(new Error('authentication failed!'));
+
+  try {
+    const decoded = await promisify(jwt.verify)(
+      token,
+      process.env.SESSION_SECRET
+    );
+
+    await User.findOne({
+      where: { id: decoded.id },
     });
+
+    next();
+  } catch {
+    next(new Error('authentication failed!'));
   }
-  next(new Error('authentication failed!'));
 };
 
 module.exports = authenticateConnection;
